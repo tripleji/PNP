f19bfc58a109f15804a6a8cc88678782cd00ac5e
tripleji <67589423+tripleji@users.noreply.github.com>
2020-12-13 18:07:55 +0900

    change dir

---
+++ TypeScript/why-ts/sample.js
@@ -0,0 +1,17 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// sum(10, '20');
+
+// @ts-check
+
+/**
+ * 
+ * @param {number} a
+ * @param {number} b
+ */
+function sum(a, b){
+    return a + b;
+}
+sum(10, 20);
